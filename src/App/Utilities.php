4393f474e37772036abe96bd2c8b5da6b8886a46
Wiktor Klonowski <wiktor.klonowski@gmail.com>
2022-03-20 13:07:01 +0100

    Add email to wdk hash conversion func

--- src/App/Utilities.php
+++ src/App/Utilities.php
@@ -3,8 +3,16 @@
 namespace App;
 
 use InvalidArgumentException;
+use SKleeschulte\Base32;
 
 class Utilities {
+    /**
+     * Return email addresses under specific domains.
+     * 
+     * @param string[] $emails Array of email addresses
+     * @param string[] $domains Array of domains
+     * @return string[] Filtered emails
+     */
     static function filterEmailAddresses($emails, $domains) {
         if (!is_array($emails)) {
             throw new InvalidArgumentException('$emails must be an array');
@@ -29,5 +37,25 @@ class Utilities {
         }
 
         return $filteredEmails;
+    }
+
+    /**
+     * Calculate Web Key Directory hash from email address
+     * 
+     * @param string $email
+     * @return string
+     */
+    static function emailToWDKHash($email) {
+        $email = strtolower($email);
+        $emailParts = explode('@', $email);
+        if (count($emailParts) != 2) {
+            throw new InvalidArgumentException('Invalid email address');
+        }
+
+        $local = $emailParts[0];
+        $sha1 = hash('sha1', $local, true);
+        $wdk = Base32::encodeByteStrToZooko($sha1);
+        
+        return $wdk;
     }
 }
